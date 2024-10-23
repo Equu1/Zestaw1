8ec1942549d7ade7d0e600465595e2be1e17ea87
Marcin Duda <dudamarcin539@gmail.com>
2024-10-23 21:06:49 +0200

    poprawka

--- 11.py
+++ 11.py
@@ -10,9 +10,9 @@ wyznacznik = a1*b2-a2*b1
 
 if wyznacznik == 0:
     if a1*c2==a2*c1 and b1*c2==b2*c1:
-        print("Układ ma nieskończenie wiele rozwiązań.")
+        print("Układ ma nieskończenie wiele rozwiązań")
     else:
-        print("Układ nie ma rozwiązań.")
+        print("Układ nie ma rozwiązań")
 else:
     x = (c1*b2-c2*b1)/wyznacznik
     y = (a1*c2-a2*c1)/wyznacznik
